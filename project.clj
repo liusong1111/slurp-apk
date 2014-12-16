b3a88057aec712805bbeff405ff57e63e6ed6da1
liusong1111 <liusong1111@gmail.com>
2014-12-16 14:58:59 +0800

    routes

--- project.clj
+++ project.clj
@@ -1,12 +1,23 @@
 (defproject slurp-apk "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
-  :ring {:handler slurp-apk.core.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+            :description "FIXME: write description"
+            :url "http://example.com/FIXME"
+            :min-lein-version "2.0.0"
+            :dependencies [[org.clojure/clojure "1.6.0"]
+                           [compojure "1.3.1"]
+                           [ring/ring-defaults "0.1.2"]
+                           [http-kit "2.1.16"]
+                           [korma "0.4.0"]
+                           ;[mysql/mysql-connector-java "5.1.26"]
+                           [org.xerial/sqlite-jdbc "3.7.2"]
+                           [com.taoensso/timbre "3.3.1"]
+                           [com.postspectacular/rotor "0.1.0"]
+                           ]
+            :plugins [[lein-ring "0.8.13"]]
+            :ring {:handler slurp-apk.core.handler/app
+                   :init slurp-apk.core.handler/init
+                   :destroy slurp-apk.core.handler/destroy
+                   }
+            :profiles
+            {:uberjar {:aot :all}
+             :dev     {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                      [ring-mock "0.1.5"]]}})
--- src/slurp_apk/core/handler.clj
+++ src/slurp_apk/core/handler.clj
@@ -1,11 +1,35 @@
 (ns slurp-apk.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [com.postspectacular.rotor :as rotor]
+            [taoensso.timbre :as timbre]))
+
+(defn init []
+  (timbre/set-config! [:timestamp-pattern] "yyyy-MM-dd HH:mm:ss")
+  (timbre/set-config!
+    [:appenders :rotor]
+    {:min-level             :info
+     :enabled?              true
+     :async?                false                           ; should be always false for rotor
+     :max-message-per-msecs nil
+     :fn                    rotor/append})
+  (timbre/set-config!
+    [:shared-appender-config :rotor]
+    {:path "production.log" :max-size (* 1024 1024) :backlog 100})
+
+  (timbre/info "slurp-apk is starting"))
+
+(defn destroy []
+  (timbre/info "slurp-apk is shutting down"))
+
+(defn accept-url [url]
+  )
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+           (GET "/" [] "Hello World")
+           (POST "/api/welcome-url" [url] (accept-url url))
+           (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
